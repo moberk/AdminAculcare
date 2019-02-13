04503484d9db8b05aaa7e5feaad4dda4ded5c9b3
“moberk” <erickhm_360@hotmail.com>
2019-02-13 01:19:47 -0600

    Modificaciones en los módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,38 +4,26 @@ import { NgModule } from '@angular/core';
 // Routes
 import {APP_ROUTES} from './app.routes';
 
+// Modules
+import { PagesModule } from './pages/pages.module';
 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphics1Component } from './pages/graphics1/graphics1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { from } from 'rxjs';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graphics1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
